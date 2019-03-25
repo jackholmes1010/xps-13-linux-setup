65ef08a7c41e9790e964a3d227abe737c4241493
Jack Holmes <jaackholmes@gmail.com>
2019-03-25 20:00:58 +1300

    Modified brightness scripts

--- volume-up.sh
+++ volume-up.sh
@@ -1,1 +1,1 @@
-pactl set-sink-volume "$(pactl list short sinks | sed -e 's,^\([0-9][0-9]*\)[^0-9].*,\1,' | head -n 1)" +5%
+pactl set-sink-volume "$(pactl list short sinks | grep RUNNING | sed -e 's,^\([0-9][0-9]*\)[^0-9].*,\1,' | head -n 1)" +5%
